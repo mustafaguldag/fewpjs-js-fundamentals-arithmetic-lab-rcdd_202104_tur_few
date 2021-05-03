bcfc25e52c92f3308e3b2cfd2c14c5d5571a09d5
mustafaguldag <guldag.mustafaa@gmail.com>
2021-05-03 12:31:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 100;
+const num4 = 96;
+
+let multiply = num1*num2
+let random = Math.ceil(Math.random());
+let mod = num3 % num4
+let max = Math.max(1, 20);
